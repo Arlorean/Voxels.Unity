35dd8c4c8fd4c357bb061fbd7da0fd37f54b516d
Adam Davidson <adam@adam-davidson.com>
2017-08-16 15:53:58 +0100

    Split shadow and main mesh into 2 game objects Store Meshes and Prefabs in a new subdirectory Update default scene to have just hemispherical lighting.

--- Assets/Voxels.Unity/Editor/VoxAssetPostprocessor.cs
+++ Assets/Voxels.Unity/Editor/VoxAssetPostprocessor.cs
@@ -1,13 +1,22 @@
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using Voxels;
 
 public class VoxAssetPostprocessor : AssetPostprocessor {
 
     public static void ImportMagicaVoxelFile(string path) {
-        var prefabPath = Path.ChangeExtension(path, ".prefab");
-        var meshPath = Path.ChangeExtension(path, ".asset");
+        var dir = Path.GetDirectoryName(path);
         var name = Path.GetFileNameWithoutExtension(path);
+
+        var meshDir = dir + "/Meshes/";
+        var prefabDir = dir + "/Prefabs/";
+        Directory.CreateDirectory(meshDir);
+        Directory.CreateDirectory(prefabDir);
+
+        var prefabPath = prefabDir+name + ".prefab";
+        var meshPath = meshDir+name + ".asset";
+        var shadowPath = meshDir+name + ".shadow.asset";
 
         var mesh = AssetDatabase.LoadAssetAtPath<Mesh>(meshPath);
         if (mesh == null) {
@@ -16,17 +25,31 @@ public class VoxAssetPostprocessor : AssetPostprocessor {
             AssetDatabase.CreateAsset(mesh, meshPath);
         }
 
+        var shadow = AssetDatabase.LoadAssetAtPath<Mesh>(shadowPath);
+        if (shadow == null) {
+            shadow = new Mesh();
+            shadow.name = name+".shadow";
+            AssetDatabase.CreateAsset(shadow, shadowPath);
+        }
+
         var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
-        if (prefab == null) {
+        if (prefab == null || !prefab.transform.Find("shadow")) {
             var gameObject = new GameObject(name);
+            var prefabShadow = new GameObject("shadow");
+            prefabShadow.transform.parent = gameObject.transform;
             prefab = PrefabUtility.CreatePrefab(prefabPath, gameObject);
             GameObject.DestroyImmediate(gameObject);
+            GameObject.DestroyImmediate(prefabShadow);
         }
 
         var voxelData = VoxelImporter.ReadMagicaVoxelFile(path);
-        VoxelImporter.VoxelsToUnity(prefab, mesh, voxelData);
+        VoxelImporter.VoxelsToUnity(prefab, mesh, voxelData, new MeshSettings { FrontFaces = true, BackFaces = true });
+
+        var shadowObject = prefab.transform.Find("shadow").gameObject;
+        VoxelImporter.VoxelsToUnity(shadowObject, shadow, voxelData, new MeshSettings { FloorShadow = true });
 
         EditorUtility.SetDirty(mesh);
+        EditorUtility.SetDirty(shadow);
         EditorUtility.SetDirty(prefab);
     }
 
--- Assets/Voxels.Unity/VoxelImporter.cs
+++ Assets/Voxels.Unity/VoxelImporter.cs
@@ -4,18 +4,16 @@ using System.Linq;
 using UnityEngine;
 using Voxels;
 
-public static class VoxelImporter {
-    static MeshSettings settings = new MeshSettings() {
-        AmbientOcclusion = true,
-    };
+using Color = Voxels.Color;
 
+public static class VoxelImporter {
     public static VoxelData ReadMagicaVoxelFile(string path) {
         using (var stream = File.OpenRead(path)) {
             return MagicaVoxel.Read(stream);
         }
     }
 
-    public static void VoxelsToUnity(GameObject go, Mesh mesh, VoxelData voxelData) {
+    public static void VoxelsToUnity(GameObject go, Mesh mesh, VoxelData voxelData, MeshSettings settings) {
         // Recenter the .vox model so the bottom is at the origin
         var matrix = Matrix4x4.Translate(new Vector3(-voxelData.size.X*0.5f, 0, -voxelData.size.Y*0.5f));
 
@@ -24,8 +22,14 @@ public static class VoxelImporter {
         mesh.Clear();
         mesh.vertices = meshBuilder.Vertices.Select(v => matrix.MultiplyPoint(new Vector3(v.X, v.Z, v.Y))).ToArray();
         mesh.normals = meshBuilder.Normals.Select(n => matrix.MultiplyVector(new Vector3(n.X, n.Z, n.Y))).ToArray();
-        mesh.colors32 = meshBuilder.Colors.Select(c => new Color32(c.R, c.G, c.B, c.A)).ToArray();
         mesh.triangles = meshBuilder.Faces.Select(f => (int)f).ToArray();
+
+        // Combine occlusion with vertex colors
+        var colors = meshBuilder.Colors;
+        var occlusion = meshBuilder.Occlusion;
+        mesh.colors32 = Enumerable.Range(0, meshBuilder.Colors.Length)
+            .Select(i => AmbientOcclusion.CombineColorOcclusion(colors[i], occlusion[i]))
+            .Select(c => new Color32(c.R, c.G, c.B, c.A)).ToArray();
 
         // Set mesh filter mesh
         var meshFilter = go.GetComponent<MeshFilter>();
@@ -39,6 +43,6 @@ public static class VoxelImporter {
         if (meshRenderer == null) {
             meshRenderer = go.AddComponent<MeshRenderer>();
         }
-        meshRenderer.sharedMaterial = Resources.Load<Material>("Standard");
+        meshRenderer.sharedMaterial = Resources.Load<Material>(settings.FloorShadow ? "StandardTransparent" : "Standard");
     }
 }
